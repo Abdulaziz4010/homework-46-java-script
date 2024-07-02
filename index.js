4c9015156127fe9612f2876b4e8753196457fd82
Abdulaziz4010 <abdulazizs707ja@gamil.com>
2024-07-02 06:09:38 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// 1. prompt da kiritilgan songacha bo'lgan juft va toq sonlar yig'indisini toping va ular o'rtasidagi farqni ham console ga chiqaring
+let input = prompt("Son kiriting");
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function sum(a) {
+//   let resault = 0;
+//   for (let i = 0; i <= a; i++) {
+//     if (a % 2 == 0) {
+//       resault += i;
+//       console.log(resault);
+//     } else if (a % 2 !== 0) {
+//       resault += i;
+//       console.log(resault);
+//     }
+//   }
+//   console.log(resault);
+//   return resault;
+// }
+
+// sum(input);
+
+// 2. shunday function yarating ki argument sifatida berilgan songacha bo'lgan sonlarning yig'indisi qaytarsin (return qilsin)
+
+function sumUpTo(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+// 3. array ni ichidagi sonlar yig'indisini topib beradigan algorithm yarating array sonlardan tashkil topsin
+
+// 4. array ichidagi juft sonlar yig'indisini topadigan algorithm yarating
+
+// 5. shunday function yaratinki agument sifatida berilgan son array ichida bor yoki yo'qligini aniqlab bersin
+
+// 6. shunday function yaratinki agument sifatida berilgan son array ichida bor bo'lsa array dan olib tashlab agar bo'lmasa array ning oxiridan qo'shib qo'ysin
+
+// 7. array ichidagi birinchi va oxirgi sonlar o'rtasidagi farqni toping farq har doim musbat son chiqishi kerak
